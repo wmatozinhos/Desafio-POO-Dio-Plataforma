3327d10cf8e3185e2c7d36df03a5a80d0777d707
Abiguelini <alvaro.jaques.biguilini@gmail.com>
2024-05-23 14:52:40 -0300

    Minha contribuicao

--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -11,10 +11,15 @@ public class Mentoria extends Conteudo{
         return XP_PADRAO + 20d;
     }
 
-    public Mentoria() {
-    }
+    //construtor na classe filho
 
-    public LocalDate getData() {
+	public Mentoria(String titulo, String descricao, LocalDate data) {
+		super(titulo, descricao);
+		this.data = data;
+	}
+
+
+	public LocalDate getData() {
         return data;
     }
 
@@ -24,10 +29,8 @@ public class Mentoria extends Conteudo{
 
     @Override
     public String toString() {
-        return "Mentoria{" +
-                "titulo='" + getTitulo() + '\'' +
-                ", descricao='" + getDescricao() + '\'' +
-                ", data=" + data +
-                '}';
+        return "Mentoria" +
+                "titulo: " + getTitulo() + 
+                ", descricao: " + getDescricao() ;
     }
 }
