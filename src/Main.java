eb0832f8de4a3331c79cc7c33c20dd0e3f05f3df
Bruno <bruno.dev90@gmail.com>
2024-06-17 21:21:58 -0300

    Inserção do método "getTempoRestante" para informar o usuário do tempo que ainda resta.

--- src/Main.java
+++ src/Main.java
@@ -44,7 +44,7 @@ public class Main {
         System.out.println("Conteúdos Inscritos Camila:" + devCamila.getConteudosInscritos());
         System.out.println("Conteúdos Concluídos Camila:" + devCamila.getConteudosConcluidos());
         System.out.println("XP:" + devCamila.calcularTotalXp());
-        System.out.println("Tempo Restante: " + bootcamp.getTempoRestante() + " Dias");
+        System.out.println("Tempo Restante: " + bootcamp.getTempoRestante() + " Dias ");
 
         System.out.println("-------");
 
@@ -59,7 +59,7 @@ public class Main {
         System.out.println("Conteúdos Inscritos João:" + devJoao.getConteudosInscritos());
         System.out.println("Conteúdos Concluidos João:" + devJoao.getConteudosConcluidos());
         System.out.println("XP:" + devJoao.calcularTotalXp());
-        System.out.println("Tempo Restante: " + bootcamp.getTempoRestante() + " Dias");
+        System.out.println("Tempo Restante: " + bootcamp.getTempoRestante() + " Dias ");
 
     }
 
