f0cac78f5fb65dc1aec9c617e4dc4315b5ccfa27
Cedric <cedricbatalha@gmail.com>
2024-06-28 19:09:05 -0300

    melhorado o texto, incluso outra especie(live) e incluso e como participante concluindo tudo no bootcamp

--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -23,10 +23,6 @@ public class Curso extends Conteudo{
 
     @Override
     public String toString() {
-        return "Curso{" +
-                "titulo='" + getTitulo() + '\'' +
-                ", descricao='" + getDescricao() + '\'' +
-                ", cargaHoraria=" + cargaHoraria +
-                '}';
+        return "Curso: " + getTitulo() + " (" + cargaHoraria + " horas) - " + getDescricao();
     }
 }
