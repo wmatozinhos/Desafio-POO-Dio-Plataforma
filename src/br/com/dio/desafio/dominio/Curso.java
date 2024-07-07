c7e5d73b55876c6484e2ee51c0c12533c0a6a28e
Rayanne de Oliveira <147012389+RayanneCOliveira@users.noreply.github.com>
2024-07-07 18:56:05 -0300

    Update Curso.java

--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -23,10 +23,8 @@ public class Curso extends Conteudo{
 
     @Override
     public String toString() {
-        return "Curso{" +
-                "titulo='" + getTitulo() + '\'' +
-                ", descricao='" + getDescricao() + '\'' +
-                ", cargaHoraria=" + cargaHoraria +
-                '}';
+        return "{" + getTitulo() + ", descrição = " + getDescricao() +
+                ", carga horária = " + cargaHoraria +
+                " horas}";
     }
 }
