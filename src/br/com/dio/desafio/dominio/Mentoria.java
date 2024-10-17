75a71620845cd9c07d73e928734cac280ca6b1c0
Rorras <rorras.n@gmail.com>
2024-10-17 13:13:46 -0300

    alterado

--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -24,10 +24,8 @@ public class Mentoria extends Conteudo{
 
     @Override
     public String toString() {
-        return "Mentoria{" +
-                "titulo='" + getTitulo() + '\'' +
-                ", descricao='" + getDescricao() + '\'' +
-                ", data=" + data +
-                '}';
+        return "\n MENTORIAS Titulo = " + getTitulo()  +
+                ", Descricao da mentoria = " + getDescricao() +
+                ", Data da mentoria = " + data ;
     }
 }
