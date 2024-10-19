b23a0766ad8866b6c3280a5346a9d25247ac53a6
Matheus <silva.dantas2@escolar.ifrn.edu.br>
2024-10-19 18:39:43 -0300

    add idade ao dev

--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -4,6 +4,8 @@ import java.util.*;
 
 public class Dev {
     private String nome;
+    private int idade;
+   
     private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
@@ -45,6 +47,15 @@ public class Dev {
     public void setNome(String nome) {
         this.nome = nome;
     }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
 
     public Set<Conteudo> getConteudosInscritos() {
         return conteudosInscritos;
